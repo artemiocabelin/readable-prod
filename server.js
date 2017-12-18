097fcb5f16c017b2bb6f852758a70e27bf6163e0
Artemio Cabelin <artemiocabelin@gmail.com>
2017-12-18 12:18:42 -0800

    production

--- server.js
+++ server.js
@@ -312,6 +312,10 @@ app.delete('/comments/:id', (req, res) => {
       )
 })
 
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build/index.html'))
+})
+
 app.listen(config.port, () => {
   console.log('Server listening on port %s, Ctrl+C to stop', config.port)
 })
